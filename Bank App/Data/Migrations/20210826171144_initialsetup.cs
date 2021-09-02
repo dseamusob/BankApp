dfd8f51c2d701fd73f0425293e6e11dbf1e4f6a1
Daniel O'Brien <dseamusob@gmail.com>
2021-09-02 09:40:21 -0400

    Add project files.

---
+++ Bank App/Data/Migrations/20210826171144_initialsetup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bank_App.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+        }
+    }
+}
---
+++ Bank App/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank_App.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Balance { get; set; }
+
+
+        public Account()
+        {
+
+        }
+    }
+
+}
